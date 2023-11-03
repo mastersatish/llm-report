01cbed9efd6268e762b0e4187dff9161023477fd
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-11-03 10:37:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ with takeaway_col[0]:
         OpenAI has become the standard for LLM apps due to its pioneering GPT research, high-quality outputs, steerability, and accessible API. Their first-mover debut of ChatGPT and large transformer-based models sparked the imagination of developers, and the world, at large.
     """, unsafe_allow_html=True)
 
-    redirect_button('Go to Top models', '#top-models')
+    redirect_button('#top-models', 'Go to Top models')
     
     #st.altair_chart(make_donut(pct_llm_models, 'LLM Models', 'blue'), use_container_width=True)
 
